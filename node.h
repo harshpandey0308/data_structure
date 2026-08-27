03b182fde5522542be55af92676706073060a457
Harsh Pandey <harshxvinod0331@gmail.com>
2026-08-27 09:11:41 +0530

    initial commit

--- node.h
+++
@@ -1,9 +0,0 @@
-#ifndef NODE_H
-#define NODE_H
-
-struct node{
-    char data;
-    struct node* next;
-};
-
-#endif
--- node1.h
+++
@@ -1,9 +0,0 @@
-#ifndef NODE1_H
-#define NODE1_H
-
-struct node{
-    int data;
-    struct node* next;
-};
-
-#endif
