9cf2fc7fecc3ac5aa4dcaa9521ccae002f8b8ed3
Harshpandey <208502446+harshpandey0308@users.noreply.github.com>
2026-09-23 04:37:52 +0000

    sliding window problem

--- deque_ll.cpp
+++ deque_ll.cpp
@@ -1,39 +1,61 @@
 #include<iostream>
 
-#define MAX_SIZE 5
+#define MAX_SIZE 3
 
 using namespace std;
 
 class SLIDING_WINDOW{
     private:
        int arr[MAX_SIZE];
-       size_t n;
-       size_t k;
+       size_t n; // size of the window
+       size_t front; // k is an index which always store front element.
+       size_t back;
 
     public : 
-       SLIDING_WINDOW(): arr[0]{0} , n(0) , k(0){}  
+       SLIDING_WINDOW(): arr{0} , n(0) , front(0) , back(MAX_SIZE-1){}  
 
        void push(int num[] , int index){
-            int j = static_cast<int>k;
-            if(j >= MAX_SIZE - 1){
+            if(n >= MAX_SIZE){
                 cout<<"window is overflow";
                 return;
             }
 
-            if(num[k] <= num[index]){
-                arr[k] = index;
+            if(n == 0){
+                arr[front] = index;
+                n++;
+                return;
             }
+
+            while(num[arr[back]] <= num[index]){
+                arr[back] = index;
+                if(back < MAX_SIZE-1){
+                    n--;
+                }
+                back--;
+            }
+
             else{
-                j++;
+                back = (back + 1)%MAX_SIZE;  // optimizing space by implementing deque rotarion
                 arr[j] = index;
+                n++;
             }
+       }
+
+       int pop_MAX(int num[]){
+           if(n == 0){
+             exit(-1);
+           }
+           int data = num[arr[front]];
+           front = (front+1)%MAX_SIZE;
+           n--;
+           return data;
        }
             
        
 
        void print(){
-        for(size_t i=0 ; i<n ; i++){
-            cout<<"the max element is : "<<arr[(k+i)%MAX_SIZE]<<'\n';
+        for(size_t i=front ; i<back ; i = (i+1)%MAX_SIZE){
+            cout<<"the max element is : "<<arr[i]<<'\n';
         }
        }
 
@@ -48,17 +70,19 @@ int main(){
 
     SLIDING_WINDOW s1;
 
-    s1.push_MAX(num , 0);
+    s1.push(num , 0);
 
-    s1.push_MAX(1);
+    s1.push(num , 1);
 
-    s1.push_MAX(2);
+    s1.push(num , 2);
 
-    int max = s1.pop_MAX();
+    s1.print();
+
+    int max = s1.pop_MAX(num);
 
     cout<<"the maximum in the sliding window is "<<max<<'\n';
 
 
-    s1.print();
+    
 
 }
